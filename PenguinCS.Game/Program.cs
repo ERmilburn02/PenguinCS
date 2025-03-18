b489866303b1046ff7097702e1abac2e2d4459fa
Eliza Milburn <ERmilburn02@outlook.com>
2025-03-18 23:51:27 +0000

    Added PenguinCS.Game Project

---
+++ PenguinCS.Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PenguinCS.Game;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        return;
+    }
+}
